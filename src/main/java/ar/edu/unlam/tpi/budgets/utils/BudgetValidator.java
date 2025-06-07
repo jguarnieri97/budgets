d7bdd45a0bd016481ba28a7ec6bc80123ed27308
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-06-07 17:12:53 -0300

    Update BudgetValidator.java

--- src/main/java/ar/edu/unlam/tpi/budgets/utils/BudgetValidator.java
+++ src/main/java/ar/edu/unlam/tpi/budgets/utils/BudgetValidator.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import ar.edu.unlam.tpi.budgets.dto.request.BudgetFinalizeRequestDto;
 import ar.edu.unlam.tpi.budgets.model.BudgetRequestEntity;
 import ar.edu.unlam.tpi.budgets.model.enums.BudgetState;
+import ar.edu.unlam.tpi.budgets.exceptions.NotFoundException;
 
 
 
@@ -15,6 +16,11 @@ public class BudgetValidator {
         Long supplierHired = request.getSupplierHired();
         if (supplierHired == null) {
             throw new IllegalArgumentException("El proveedor contratado no puede ser null");
+        }
+
+        //verificar que el proveedor contratado sea un proveedor valido de la lista de proveedoresde esta solicitud
+        if (!budgetRequest.getBudgets().stream().anyMatch(budget -> budget.getSupplierId().equals(supplierHired))) {
+            throw new NotFoundException("El proveedor contratado no es un proveedor valido de esta solicitud");
         }
     
         budgetRequest.getBudgets().forEach(budget -> {
